9776daa15481c1db859504dc5ce9a17d31e1e9f7
Your Name <you@example.com>
2025-12-19 20:07:09 +0300

    fead: we started the first task

---
+++ homework/task-1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Введите число 1: "))
+num2 = int(input("Введите число 2: "))
+num3 = int(input("Введите число 3: "))
+sum = num1 + num2 + num3
+multip = num1 * num2 * num3
+print(f"Сумма чисел: {sum}")
+print(f"Произведение: {multip}")
