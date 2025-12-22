1ef5b8622c60de31454f7dc2ba20ea36ce5e4ad6
bmarinaleonteva94-code <bmarina.leonteva.94@gmail.com>
2025-12-22 19:21:22 +0300

    задание по 2 модулю

---
+++ classwork/task_2.py
@@ -0,0 +1,8 @@
+number = int(input("Введите четырехзначное число: "))    #4563
+num1 = number // 1000     #4
+num2 = number //100 % 10
+num3 = number // 10 %10
+num4 = number %10
+print(num1 * num2 * num3 * num4)
+
+
---
+++ classwork/task_4.py
@@ -0,0 +1,4 @@
+triangle_base = float(input("Введите размер основания треугольника: "))
+triangle_height = float(input("Введите размер высоты треугольника: "))
+square = 0.5 * triangle_base * triangle_height
+print(square)
