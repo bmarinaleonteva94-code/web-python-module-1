5cde2cede527390abccb7ba78a33f8eff02217f3
bmarinaleonteva94-code <bmarina.leonteva.94@gmail.com>
2026-01-13 14:52:53 +0300

    aa

--- module-1/homework/task_2.py
+++ module-1/homework/task_2.py
@@ -1,1 +1,5 @@
-print("to be\nor not\nto be")
+salary = float(input("Введите зарплату за месяц: "))
+loan_payment = float(input("Платеж по кредиту: "))
+utilities_payment = float(input("Платеж за коммунальные услуги: "))
+balance = salary - loan_payment - utilities_payment
+print(balance)
