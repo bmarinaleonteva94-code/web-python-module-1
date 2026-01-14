b33cdaae959d97275e4b815d46f7ea0cd5d04a78
bmarinaleonteva94-code <bmarina.leonteva.94@gmail.com>
2026-01-14 20:49:45 +0300

    Изучение списков в Python

--- module-1/classwork/task_1.py
+++ module-1/classwork/task_1.py
@@ -1,9 +1,3 @@
-num1 = int(input("Введите число от 1 до 9: "))
-num2 = int(input("Введите число от 1 до 9: "))
-num3 = int(input("Введите число от 1 до 9: "))
-res = num1*100 + num2*10 + num3
-print(res)
-
-
-
-
+words = ['apple', 'banana', 'car', 'python', 'cat']
+result = [word for word in words if len(word)>=4]
+print(result)
